4fd8803cd87d0d07eb7768629ec7630ca2bbe388
Gamerz99 <muneshtharinda60@gmail.com>
2019-08-18 09:55:57 +0530

    ff

--- twitter_stream.py
+++ twitter_stream.py
@@ -102,8 +102,8 @@ class TweetAnalyzer():
         write_data = {}
         write_data['data'] = []
 
-        if os.path.exists("/app/tweets.json"):
-            os.remove("tweets.json")
+        if os.path.exists("app/tweets.json"):
+            os.remove("app/tweets.json")
         try:
             with open(self.fetched_tweets, 'a') as tf:
                 for tweet in tweets:
